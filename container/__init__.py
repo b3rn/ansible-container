2d16a059426bde8fb325bcf79812086f97452d94
ber <ber@redhat.com>
2017-10-19 14:42:47 -0500

    upstream sync

--- container/__init__.py
+++ container/__init__.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-<<<<<<< HEAD
-__version__ = '0.9.2'
-=======
 __version__ = '0.9.3rc0'
->>>>>>> upstream/develop
 
 import os
 import functools
